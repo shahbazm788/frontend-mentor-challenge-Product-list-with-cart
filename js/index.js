75f15061abc55a22688fbf034572eb041c4afe46
shahbazm788 <shahbazm788@gmail.com>
2024-10-17 09:54:05 +0500

    update

--- js/index.js
+++ js/index.js
@@ -1,56 +1,45 @@
 
-let arr = [];
+let arr = []; //init array for products card which we will create afrer fetching
 
 
-// storing all items in items on fetch for compearing 
-let items = [];
 
-// here will comes all items who will added by user 
-let cartItems = [];
+let items = []; // storing all items in items on fetch for compearing 
 
-let cartItemsHtmlCode = [];
-let productsOuter = document.querySelector('.products');
-const cart_outer = document.getElementById('cart_outer');
 
-const cart_title = document.getElementById('cart_title');
-const cart_inner = document.getElementById('cart_inner');
-const submit_order = document.getElementById('submit_order');
-const inc_btn = document.getElementById("inc_btn");
-const prompt_box = document.getElementById('prompt_box');
-const prompt_inner = document.getElementById('prompt_inner');
-const confirom_inner = document.getElementById('confirom_inner');
-const total_confirom = document.getElementById("total_confirom");
+let cartItems = [];// here will comes all items which will added by user in cart
 
-let screenWidth = window.innerWidth;
+let cartItemsHtmlCode = []; // code which will be use ase innerHtml
+let productsOuter = document.querySelector('.products'); // selecting product container 
+const cart_outer = document.getElementById('cart_outer'); //selecting cart container 
+
+const cart_title = document.getElementById('cart_title'); // selecting text element of cart headding
+const cart_inner = document.getElementById('cart_inner'); // selecting cart inner where All user added items will appear 
+const submit_order = document.getElementById('submit_order');// selecting submit element 
+const inc_btn = document.getElementById("inc_btn"); // selecting increment button
+const prompt_box = document.getElementById('prompt_box'); // selecting prompt box
+const prompt_inner = document.getElementById('prompt_inner'); // selecting prompt box inner
+const confirom_inner = document.getElementById('confirom_inner');// selecting confirom box 
+const total_confirom = document.getElementById("total_confirom"); // selecting element where total amount will shows
+// getting screen width and height for representing images dynamically 
+let screenWidth = window.innerWidth; 
 let screenHeight = window.innerHeight;
 
+
+// setting initial "0" cart item on load
 cart_title.innerText =`Your Cart (${cartItems.length})`;
 
 
-function totalConfirom (){
-  let total = 0;
-  cartItems.map((item,i) => {
-    return total = total + (item.qty * item.price);
-  })
-totalConfirom.innerHTML = ` <div class="totle_info">
-              <p class="total_info_p">Order Total</p>
-              <p class="total_amount">\$${total}</p>
-            </div>
-           `;
-}
-
-// function btns (e)  {
-//   console.log(e.target)
-// }
+// getting products data from api (data.json file)
 const apiData = async () => {
   const res = await fetch('data.json');
   const data = await res.json();
   items = data;
   
   data.map((item, i) => {
-    // console.log(items)
-    // const itemName = item.name;
+    //setting image path for different devices witdth size
       let img =  screenWidth < 768 ? item.image.mobile : screenWidth > 768 && screenWidth < 992 ? item.image.tablet : item.image.desktop;
+    
+    // pushing product card in array which we defined on top
     arr.push(
       `<div class="single_product" id='product${i}'>
             <div class="cord">
@@ -81,42 +70,75 @@ const apiData = async () => {
 
   })
 
+// seting data on html tag where we want to show all products
   productsOuter.innerHTML = arr;
 
 }
 
+// calling our main function to get data from api 
+let commingData = apiData();
 
+// setting up total amount of cart items
+function totalConfirom (){
+  let total = 0;
+  cartItems.map((item,i) => {
+    return total = total + (item.qty * item.price);
+  })
+  
+  // showing total amount of cart items to user
+totalConfirom.innerHTML = ` <div class="totle_info">
+              <p class="total_info_p">Order Total</p>
+              <p class="total_amount">\$${total}</p>
+            </div>
+           `;
+}
+
+
+
+
+ // creating function for increase quantity 
 function incFunc(i) {
+  // getting element where we show how much quantity user selected of an item
   const counteId = document.getElementById(`counter${i}`);
+  
+  // getting element inner text we will use it to compearing 
   const item_name = document.getElementById(`item_name${i}`);
 
+// getting element where we set crunt quantity of item and after updating we will set updated numbers 
   const num = parseInt(counteId.innerText);
-
+// increasing item quantity 
 cartItems.map((item,i) => {
   item.name === item_name.innerText ? ([...cartItems,item.qty=item.qty+1],counteId.innerText=item.qty): '';
 
-})
+});
+// re-rendere cart items, quantity, total prices etc👇
 setCartDivData();
 grandTotal();
 totalCartItems();
 }
 
 
+// setting detriment function  
 function decFunc(i) {
-
+// getting all necessary elements 
   const cart_btn = document.getElementById(`cart_btn_id${i}`);
   const counter_btn = document.getElementById(`counter_btn_id${i}`);
   const item_name = document.getElementById(`item_name${i}`);
   const counteId = document.getElementById(`counter${i}`);
   
+  
+  // applying logic to chack if item quantity already is equal 1
 if(parseInt(counteId.innerText) <= 1){
   cartItems = cartItems.filter((item,i) => {
     return item.name !== item_name.innerText;
     });
+    // if condition true we will hide increment & decrement buttons because its our design requirement 
     counteId.innerText=0 ;
       cart_btn.classList.remove('hide');
     counter_btn.classList.add('hide');
 }
+
+// if condition false we will decrease item quantity by 1
 else{
   cartItems.map((item,i) => {
     if(item.name == item_name.innerText){
@@ -125,13 +147,15 @@ else{
     
     })
     counteId.innerText = counteId.innerText - 1;
-  console.log(cartItems)
 }
-
+// updating again cart data
 setCartDivData();
 cartItems !== 0 ? grandTotal() : '';
 totalCartItems();
 }
+
+
+// setting up logics for first time adding item in cart
 function hideBtn(i) {
   const cruntItem = document.getElementById(`cart_btn_id${i}`);
   const counter_btn_id = document.getElementById(`counter_btn_id${i}`);
@@ -145,7 +169,7 @@ function hideBtn(i) {
   let count = parseInt(counter.innerText);
   cruntItem.classList.add('hide');
   counter_btn_id.classList.remove('hide');
-  // counter.innerText = count + 1;
+
   const filterdItem = items.filter((item,i) =>{
     return item.name === item_name;
   });
@@ -158,16 +182,15 @@ function hideBtn(i) {
   };
    cartItems.push(addAbleItem);
   counter.innerText = addAbleItem.qty;
-  console.log(addAbleItem)
  setCartDivData();
  grandTotal();
  cartItems !== ' '? totalCartItems(): '';
 }
-let commingData = apiData();
 
+// creating function which we used inside increments & decrement functions to show cart data 
 function setCartDivData (){
   let cartDivInnerText = cartItems.length === 0 ? `<div class="empty_cart_img_div"><img class="empty_cart_img" src="/assets/images/illustration-empty-cart.svg" /></div>
-            <p class="empty_cart_text">Your Added Items Will appear here</p>`:cartItems.map((item,i)=> {
+            <p class="empty_cart_text">Your Added Items Will appear here</p>`:cartItems.map((item,i) => {
               let n = item.name;
    return ` <div class="cart_item">
               <div class="cart_item_left">
@@ -187,13 +210,19 @@ function setCartDivData (){
  });
  cart_inner.innerHTML = cartDivInnerText;
 }
+
+// calling on load
 setCartDivData();
+
+
+// creating function to calculate total amount of user cart
 function grandTotal (){
   let total = 0;
   cartItems.map((item,i) => {
     return total = total + (item.qty * item.price);
   })
 
+// setting up on cart element 
 if(cartItems.length >  0){
   submit_order.innerHTML = ` <div class="totle_info">
               <p class="total_info_p">Order Total</p>
@@ -211,7 +240,7 @@ else{
 }
 }
 
-
+// creating function to showing how much items inside our cart
 function totalCartItems (){
   let tmp = 0;
   cartItems.map((item,i) => {
@@ -220,8 +249,8 @@ function totalCartItems (){
   cart_title.innerText = `Your Cart (${tmp})`
 }
 
+// creating function to remove complete item from cart
 function removeItemFromCart (i){
-  // console.log(index)
 let targetItem = cartItems[i];
 let counter = targetItem.counter;
 
@@ -231,7 +260,7 @@ const cruntItem = document.getElementById(`cart_btn_id${counter}`);
 const counter_btn_id = document.getElementById(`counter_btn_id${counter}`);
 
 
-counterElement.innerText =0;
+counterElement.innerText = 0;
 cruntItem.classList.remove('hide');
 counter_btn_id.classList.add('hide');
 cartItems = cartItems.filter((item, i) => {
@@ -239,10 +268,11 @@ cartItems = cartItems.filter((item, i) => {
 });
 setCartDivData();
 cartItems !== 0 ? grandTotal() : '';
-console.log(cartItems)
 
 }
 
+
+// seting up to opne prompt box on confirmation order
 function confiromOrder (){
   prompt_box.classList.remove('hide');
  let confiromOrder = cartItems.map((item,i)=> {
@@ -286,16 +316,13 @@ confirom_inner.innerHTML = outerBox + ` <div class="totle_info">
             </div>
            `;
 }
+
+// creating function to reset everything on order confirmation 
 function emptyCart (){
   productsOuter.innerHTML = arr;
   cartItems = [];
-  console.log(cartItems)
-  cartItems !== 0 ? grandTotal() : '';
+ grandTotal();
 totalCartItems();
 setCartDivData();
 prompt_box.classList.add('hide');
-
-
-
-  // window.location.reload()
 }
